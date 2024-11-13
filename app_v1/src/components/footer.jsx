fc9178aeed27a2bab6036e747d60cd55de148602
Rohit Mahant <rohitsinghmahant707@gmail.com>
2024-11-13 15:48:00 -0500

    fixed deployment issue

--- app_v1/src/components/footer.jsx
+++ app_v1/src/components/footer.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 export default function Footer() {
     return (
       <div className="bg-white text-gray-700 font-cocoRegular py-8">
@@ -11,9 +12,9 @@ export default function Footer() {
             <div>
               <h3 className="text-md font-cocoBold mb-4">Company</h3>
               <ul className="text-sm">
-                <li className="mb-2"><a href="/" className="hover:text-gray-300">About Us</a></li>
-                <li className="mb-2"><a href="/services" className="hover:text-gray-300">Services</a></li>
-                <li className="mb-2"><a href="/contact" className="hover:text-gray-300">Contact</a></li>
+                <li className="mb-2"><Link href="/" className="hover:text-gray-300">About Us</Link></li>
+                <li className="mb-2"><Link href="/retail" className="hover:text-gray-300">Services</Link></li>
+                <li className="mb-2"><Link href="/contact" className="hover:text-gray-300">Contact</Link></li>
               </ul>
             </div>
   
@@ -21,27 +22,27 @@ export default function Footer() {
             <div>
               <h3 className="text-md font-cocoBold mb-4">Products</h3>
               <ul className="text-sm">
-                <li className="mb-2"><a href="/ip-cameras" className="hover:text-gray-300">IP Cameras</a></li>
-                <li className="mb-2"><a href="/hd-cameras" className="hover:text-gray-300">HD Cameras</a></li>
-                <li className="mb-2"><a href="/nvr" className="hover:text-gray-300">NVR</a></li>
-                <li className="mb-2"><a href="/dvr" className="hover:text-gray-300">DVR</a></li>
-                <li className="mb-2"><a href="/hard-drives" className="hover:text-gray-300">Hard Drives</a></li>
+                <li className="mb-2"><Link href="/ip-cameras" className="hover:text-gray-300">IP Cameras</Link></li>
+                <li className="mb-2"><Link href="/hd-cameras" className="hover:text-gray-300">HD Cameras</Link></li>
+                <li className="mb-2"><Link href="/nvr" className="hover:text-gray-300">NVR</Link></li>
+                <li className="mb-2"><Link href="/dvr" className="hover:text-gray-300">DVR</Link></li>
+                <li className="mb-2"><Link href="/hard-drives" className="hover:text-gray-300">Hard Drives</Link></li>
               </ul>
             </div>
   
-            {/* Column 3: Social Media */}
+            {/* Column 3: Social MediLink */}
             <div>
               <h3 className="text-md font-cocoBold mb-4">Follow Us</h3>
               <div className="flex justify-center space-x-6">
-                <a href="https://facebook.com" className="hover:text-gray-300">
+                <Link href="https://facebook.com" className="hover:text-gray-300">
                   <i className="fab fa-facebook-f"></i>
-                </a>
-                <a href="https://twitter.com" className="hover:text-gray-300">
+                </Link>
+                <Link href="https://twitter.com" className="hover:text-gray-300">
                   <i className="fab fa-twitter"></i>
-                </a>
-                <a href="https://instagram.com" className="hover:text-gray-300">
+                </Link>
+                <Link href="https://instagram.com" className="hover:text-gray-300">
                   <i className="fab fa-instagram"></i>
-                </a>
+                </Link>
               </div>
             </div>
           </div>
